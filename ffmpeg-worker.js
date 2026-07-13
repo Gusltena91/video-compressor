707c9b9ea3671d36886c17163916e0eb6f679e69
Gusltena91 <kurgen90@gmail.com>
2026-07-13 14:27:24 -0600

    Delete ffmpeg-worker.js

--- ffmpeg-worker.js
+++
@@ -1,3 +0,0 @@
-// Local proxy for FFmpeg class worker - avoids cross-origin Worker restriction
-// The actual code is loaded from CDN via importScripts (which allows cross-origin)
-importScripts('https://cdn.jsdelivr.net/npm/@ffmpeg/ffmpeg@0.12.10/dist/umd/814.ffmpeg.js');
